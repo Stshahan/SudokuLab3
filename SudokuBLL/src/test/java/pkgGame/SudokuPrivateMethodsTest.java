98d7fde9b33dccbef8cda1401a00f28cb5e9fa5e
stsha <stsha@10.0.0.193>
2019-04-08 13:04:23 -0400

    STS push

--- SudokuBLL/src/test/java/pkgGame/SudokuPrivateMethodsTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuPrivateMethodsTest.java
@@ -13,25 +13,10 @@ public class SudokuPrivateMethodsTest {
 
 	@Test
 	public void FillDiagonalRegions() throws Exception {
-		int[][]emptyPuzzle=new int[9][9];
-		Sudoku testPuzzle = new Sudoku(emptyPuzzle);
-		int[] emptyRegion=new int[] {0,0,0,0,0,0,0,0,0};
-		for(int i = 0; i < 9; i++) {
-			testPuzzle.setRegion(i, emptyRegion);
-		}
-		testPuzzle.testFillDiagonal();
+		Sudoku testPuzzle = new Sudoku(9);
 		
-		//assertEquals(testPuzzle.hasDuplicates(),false);
+		testPuzzle.fillDiagonalRegions();
 		
-		int[] region0=testPuzzle.getRegion(0);
-		int[] region4=testPuzzle.getRegion(4);
-		int[] region8=testPuzzle.getRegion(8);
-		int[] testRegion=new int[] {1,2,3,4,5,6,7,8,9};
-		assertEquals(testPuzzle.hasAllValues(region0,testRegion ),true);
-		assertEquals(testPuzzle.hasAllValues(region4,testRegion ),true);
-		assertEquals(testPuzzle.hasAllValues(region8,testRegion ),true);
-		
-		testPuzzle.PrintPuzzle();
 	}
 
 }
